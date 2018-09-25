52c5981881f6216cc0964f907316936e0b49a0c0
Naoloco <43401389+Naoloco@users.noreply.github.com>
2018-09-25 18:54:38 +0800

    Initial commit

---
+++ Assets/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    Rigidbody playRigidbody;
+    float moveX, moveZ;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        playRigidbody = GetComponent<Rigidbody>();
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Movementcontroller();
+
+    }
+
+    void Move()
+    {
+        moveX = Input.GetAxis("Horizontal");
+        moveZ = Input.GetAxis("Vertical");
+        playRigidbody.AddForce(new Vector3(speed * moveX, 0, speed * moveZ));
+    }
+
+    void Movementcontroller()
+    {
+        if(!talkthing.isTalking)
+        {
+            playRigidbody.constraints = RigidbodyConstraints.FreezePositionY;
+            Move();
+        }
+        else
+        {
+            //RigidbodyConstraints abccc = GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
+            playRigidbody.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionZ;
+        }
+    }
+}
---
+++ Assets/talkthing.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class talkthing : MonoBehaviour {
+    public static Flowchart FlowchartManager;
+    public Flowchart talkFlowchart;
+    public string onCollosionEnter;
+    public string onMouseDown;
+
+	// Use this for initialization
+	void Awake () {
+        FlowchartManager = GameObject.Find("對話管理器").GetComponent<Flowchart>();
+	}
+
+    public static bool isTalking
+    {
+        get
+        {
+            return FlowchartManager.GetBooleanVariable("對話中");
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(UnityEngine.Collision other)
+    {
+        if(other.gameObject.CompareTag("player"))
+        {
+            Block talkblock = talkFlowchart.FindBlock(onCollosionEnter);
+            talkFlowchart.ExecuteBlock(talkblock);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        Block talkBlock = talkFlowchart.FindBlock(onMouseDown);
+        talkFlowchart.ExecuteBlock(talkBlock);
+    }
+}
